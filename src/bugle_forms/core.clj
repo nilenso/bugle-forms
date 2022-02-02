e8b643142383376f1d230fb1f0374945a1974cfc
Atharva Raykar <atharva@nilenso.com>
2022-02-02 14:43:38 +0530

    add ring scaffolding

--- src/bugle_forms/core.clj
+++ src/bugle_forms/core.clj
@@ -1,7 +1,28 @@
 (ns bugle-forms.core
+  (:require [ring.adapter.jetty :as raj])
   (:gen-class))
+
+(defn hello-world [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body
+   "<iframe src='//commons.wikimedia.org/wiki/File:FirstCall.ogg?embedplayer=yes'
+   width='300' height='20' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen>
+   </iframe>"})
+
+(defonce server (atom nil))
+
+(defn start-server []
+  (reset! server (raj/run-jetty hello-world
+                                {:port 8080
+                                 :join? false})))
+
+(defn stop-server []
+  (when @server (.stop @server))
+  (reset! server nil))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "🎺🎺🎺🎺🎺")
+  (start-server))
