3b275514f0ca129e701ab2e3f435692f4ec8ed45
Atharva Raykar <atharva@nilenso.com>
2022-03-22 11:21:04 +0530

    tests: create factories for randomly generating entities
    We currently create generators for users, forms and their corresponding
    form params.

---
+++ test/bugle_forms/factories.clj
@@ -0,0 +1,38 @@
+(ns bugle-forms.factories
+  (:require
+   [bugle-forms.specs :as specs]
+   [clojure.spec.alpha :as s]
+   [clojure.spec.gen.alpha :as gen]))
+
+(defn user
+  "Randomly generate a user.
+  Optionally takes a map of overrides."
+  ([] (user {}))
+  ([overrides]
+   (gen/generate
+    (s/gen ::specs/user-account
+           (into {} (map (fn [[k v]]
+                           {k (fn [] (gen/return v))})
+                         overrides))))))
+
+(defn form
+  "Randomly generate a form.
+  Optionally takes a map of overrides."
+  ([] (form {}))
+  ([overrides]
+   (gen/generate
+    (s/gen ::specs/form
+           (into {} (map (fn [[k v]]
+                           {k (fn [] (gen/return v))})
+                         overrides))))))
+
+(defn create-form-params
+  "Randomly generate form params for creating form.
+  Optionally takes a map of overrides."
+  ([] (create-form-params {}))
+  ([overrides]
+   (gen/generate
+    (s/gen ::specs/form-creation-form
+           (into {} (map (fn [[k v]]
+                           {k (fn [] (gen/return v))})
+                         overrides))))))
