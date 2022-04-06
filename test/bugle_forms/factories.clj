d00738e41e303a4989b79a0b8b3f3083ee581e4b
Atharva Raykar <atharva@nilenso.com>
2022-04-06 15:18:07 +0530

    factories: refactor to reduce repetition
    We use a macro where we declare the name of the factory and the spec of
    what it generates.

--- test/bugle_forms/factories.clj
+++ test/bugle_forms/factories.clj
@@ -4,56 +4,40 @@
    [clojure.spec.alpha :as s]
    [clojure.spec.gen.alpha :as gen]))
 
-(defn user
-  "Randomly generate a user.
-  Optionally takes a map of overrides."
-  ([] (user {}))
-  ([overrides]
-   (gen/generate
-    (s/gen ::specs/user-account
-           (into {} (map (fn [[k v]]
-                           {k (fn [] (gen/return v))})
-                         overrides))))))
+(defn make-factory
+  "Make factory for a given spec.
+  The returned factory takes a map of overrides, which are checked against the
+  spec."
+  [spec]
+  (fn factory-fn
+    ([]
+     (factory-fn {}))
+    ([overrides]
+     (gen/generate
+      (s/gen spec
+             (into {} (map (fn [[k v]]
+                             {k (fn [] (gen/return v))})
+                           overrides)))))))
 
-(defn form
-  "Randomly generate a form.
-  Optionally takes a map of overrides."
-  ([] (form {}))
-  ([overrides]
-   (gen/generate
-    (s/gen ::specs/form
-           (into {} (map (fn [[k v]]
-                           {k (fn [] (gen/return v))})
-                         overrides))))))
+(defmacro make-factories
+  "Create factory definitions from a list of factory declarations."
+  [factory-decls]
+  `(do ~@(map (fn [{:keys [name spec] :as _factory-decl}]
+                `(def ~name
+                   ~(str "Randomly generates a " name ". "
+                         "Optionally takes a map of overrides.")
+                   (make-factory ~spec)))
+              factory-decls)))
 
-(defn create-form-params
-  "Randomly generate form params for creating form.
-  Optionally takes a map of overrides."
-  ([] (create-form-params {}))
-  ([overrides]
-   (gen/generate
-    (s/gen ::specs/form-creation-form
-           (into {} (map (fn [[k v]]
-                           {k (fn [] (gen/return v))})
-                         overrides))))))
-(defn question
-  "Randomly generate a question.
-  Optionally takes a map of overrides."
-  ([] (question {}))
-  ([overrides]
-   (gen/generate
-    (s/gen ::specs/question
-           (into {} (map (fn [[k v]]
-                           {k (fn [] (gen/return v))})
-                         overrides))))))
-
-(defn question-form-params
-  "Randomly generate form params for creating form.
-  Optionally takes a map of overrides."
-  ([] (question-form-params {}))
-  ([overrides]
-   (gen/generate
-    (s/gen ::specs/add-question-form
-           (into {} (map (fn [[k v]]
-                           {k (fn [] (gen/return v))})
-                         overrides))))))
+(make-factories [{:name user
+                  :spec ::specs/user-account}
+                 {:name form
+                  :spec ::specs/form}
+                 {:name create-form-params
+                  :spec ::specs/form-creation-form}
+                 {:name question
+                  :spec ::specs/question}
+                 {:name question-form-params
+                  :spec ::specs/add-question-form}
+                 {:name answer
+                  :spec ::specs/answer}])
